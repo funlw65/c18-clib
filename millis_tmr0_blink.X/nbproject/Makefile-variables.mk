94bfa60ce426fa9f4404f1f3c613da5e880f134e
funlw65@gmail.com <funlw65@gmail.com@b653bcc6-8b1d-dca8-e015-c6e6dec331c3>
2014-12-09 15:36:19 +0000

    Demo App for millis (Arduino like) non-blocking delays

---
+++ millis_tmr0_blink.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=millis_tmr0_blink.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/millis_tmr0_blink.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=millistmr0blink.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/millistmr0blink.x.tar
