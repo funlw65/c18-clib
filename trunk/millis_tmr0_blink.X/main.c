7383e7812480fa7a8fd5a1268daecc30d6e5e641
funlw65@gmail.com <funlw65@gmail.com@b653bcc6-8b1d-dca8-e015-c6e6dec331c3>
2014-12-09 15:36:19 +0000

    Demo App for millis (Arduino like) non-blocking delays

---
+++ trunk/millis_tmr0_blink.X/main.c
@@ -0,0 +1,71 @@
+/*
+ * File:   main.c
+ * Author: texan
+ *
+ * Created on June 21, 2014, 4:18 AM
+ */
+
+//#define NOBOOT 1 // uncomment if you don't use a bootloader
+
+// enable one of the following seven boards
+#define ROSSO_SER 1 //(18F46K22, 16MHz crystal, 64MHz, BootBTN = RA4, BootLED = RC2)
+//#define PINGUINOP8B2_HID 2 //(18F4550, 20MHz crystal, 48MHz, BootBTN and BootLED uses RC2)
+//#define PINGUINOP8B2_CDC 3 //(18F4550, 20MHz crystal, 48MHz, BootBTN and BootLED uses RC2)
+//#define PINGUINOP8B3_HID 4 //(18F4550, 20MHz crystal, 48MHz, BootBTN = RA4, BootLED = RC2)
+//#define PINGUINOP8B3_CDC 5 //(18F4550, 20MHz crystal, 48MHz, BootBTN = RA4, BootLED = RC2)
+//#define FREEJALDUINO_CDC 6 //(18F2550, 20MHz crystal, 48MHz, UserLED1 = RA4, UserLED2 = RC2)
+//#define FREEJALDUINO_HID 7 //(18F2550, 20MHz crystal, 48MHz, UserLED1 = RA4, UserLED2 = RC2)
+
+#include <GenericTypeDefs.h>
+// disable following line if your hardware differs
+#define ONBOARD 1 // enable the default onboard definitions
+#include <rosso.h> // processor type, speed, configuration bits, hardware, app_offset.
+#define TMR0_MILLIS
+#include <rosso_isr_delays.h>
+
+// declaring the headers for the interrupt functions
+void
+interrupt
+high_isr(void);
+
+void
+interrupt low_priority
+low_isr(void);
+
+UINT32 previous,current;
+
+void main() {
+    AllDigital();
+#ifdef ONBOARD
+    OnBoardLED_dir    = 0; //output
+    OnBoardButton_dir = 1; //input
+    OnBoardLED = 0;
+#endif
+    ei(); // enable general interrupts
+    tmr0_isr_init();
+    previous = 0;
+    while (1) {
+        current = tmr0_millis();
+        if((current - previous) >= 250){
+            previous = current;
+            OnBoardLED = ~OnBoardLED;
+        }
+    }
+}
+
+
+void
+interrupt
+high_isr(void) {
+    //add here any high priority interrupt management functions
+    tmr0_isr_intr();
+}
+
+
+void
+interrupt low_priority
+low_isr(void) {
+    //add here any low priority interrupt management functions
+
+}
+
