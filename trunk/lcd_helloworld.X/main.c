5d13340aa4d0356d692c6e352afcc902530d0b72
funlw65@gmail.com <funlw65@gmail.com@b653bcc6-8b1d-dca8-e015-c6e6dec331c3>
2014-12-17 01:33:10 +0000

    Added LCD example (Hello World! classic). Both XC8 and C18 Microchip compilators gets beaten by JALv2 compiler at smallest code size.

---
+++ trunk/lcd_helloworld.X/main.c
@@ -0,0 +1,140 @@
+/* 
+ * File:   main.c
+ * Author: Worker
+ *
+ * Created on December 16, 2014, 9:34 PM
+ */
+
+#include <GenericTypeDefs.h>
+//#define NOBOOT 1 // uncomment if you don't use a bootloader
+// enable one of the following seven boards
+#define ROSSO_SER 1 //(18F46K22, 16MHz crystal, 64MHz, BootBTN = RA4, BootLED = RC2)
+//#define PINGUINOP8B2_HID 2 //(18F4550, 20MHz crystal, 48MHz, BootBTN and BootLED uses RC2)
+//#define PINGUINOP8B2_CDC 3 //(18F4550, 20MHz crystal, 48MHz, BootBTN and BootLED uses RC2)
+//#define PINGUINOP8B3_HID 4 //(18F4550, 20MHz crystal, 48MHz, BootBTN = RA4, BootLED = RC2)
+//#define PINGUINOP8B3_CDC 5 //(18F4550, 20MHz crystal, 48MHz, BootBTN = RA4, BootLED = RC2)
+//#define FREEJALDUINO_CDC 6 //(18F2550, 20MHz crystal, 48MHz, UserLED1 = RA4, UserLED2 = RC2)
+//#define FREEJALDUINO_HID 7 //(18F2550, 20MHz crystal, 48MHz, UserLED1 = RA4, UserLED2 = RC2)
+// disable following line if your hardware differs
+#define ONBOARD 1 // enable the default onboard definitions
+#include <rosso.h> // processor type, speed, configuration bits, hardware, app_offset.
+#include <rosso_delays.h>
+#include <rosso_lcd4.h>
+#include <rosso_conversion.h>
+
+UINT8 s[4];
+
+// declaring the headers for the interrupt functions
+void
+#ifdef __XC8
+interrupt
+#endif
+high_isr(void);
+
+void
+#ifdef __XC8
+interrupt low_priority
+#endif
+low_isr(void);
+
+#ifdef __18CXX
+#ifndef NOBOOT
+#pragma romdata bootloader = 0x6
+const rom char bootloader[APP_START - 0x6];
+extern void _startup(void);
+
+#pragma code AppVector = APP_START
+
+void AppVector(void) {
+    _asm GOTO _startup _endasm
+}
+
+#pragma code AppHighIntVector = APP_HINT
+
+void AppHighIntVector(void) {
+    _asm
+    GOTO high_isr // branch to the high_isr()
+            _endasm
+}
+
+#pragma code AppLowIntVector = APP_LINT
+
+void low_vector(void) {
+    _asm
+    GOTO low_isr // branch to the low_isr()
+            _endasm
+}
+#endif
+
+#pragma code  // return to the default // code section
+#endif
+
+void main() {
+    UINT8 i = 0;
+    UINT8 counter = 0;
+    AllDigital(); // all pins digital
+#ifdef ONBOARD
+    OnBoardLED_dir = 0; //output
+    OnBoardButton_dir = 1; //input
+    OnBoardLED = 0;
+#endif
+    //ei(); // enable general interrupts if needed
+    // Add other initializations you may have...
+    lcd_init(LCD_HD44780);
+    // signal the start four times
+    for (i = 0; i < 4; i++) {
+        OnBoardLED = 1;
+        delay_100ms();
+        delay_100ms();
+        OnBoardLED = 0;
+        delay_100ms();
+        delay_100ms();
+    }
+    lcd_cursor_position(0, 0);
+    _lcd_write_data('H');
+    _lcd_write_data('e');
+    _lcd_write_data('l');
+    _lcd_write_data('l');
+    _lcd_write_data('o');
+    _lcd_write_data(' ');
+    _lcd_write_data('W');
+    _lcd_write_data('o');
+    _lcd_write_data('r');
+    _lcd_write_data('l');
+    _lcd_write_data('d');
+    _lcd_write_data('!');
+    while (1) {
+        // Add your repeating code...
+        counter += 1;
+        lcd_cursor_position(1, 0);
+        byte2dec(counter, s);
+        for (i = 0; i < 3; i++) _lcd_write_data(s[i]);
+        delay_150ms();
+        delay_150ms();
+    }
+}
+
+#ifdef __18CXX
+#pragma interrupt high_isr
+#endif
+
+void
+#ifdef __XC8
+interrupt
+#endif
+high_isr(void) {
+    //
+}
+
+#ifdef __18CXX
+#pragma interruptlow low_isr
+#endif
+
+void
+#ifdef __XC8
+interrupt low_priority
+#endif
+low_isr(void) {
+    //
+}
+
