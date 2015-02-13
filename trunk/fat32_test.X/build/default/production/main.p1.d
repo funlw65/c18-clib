d8c08c55515632614adeac8b8f946d0bf35dbf9a
funlw65@gmail.com <funlw65@gmail.com@b653bcc6-8b1d-dca8-e015-c6e6dec331c3>
2015-02-13 23:14:33 +0000

    Added a test program for FAT32 - not tested yet on real hardware

---
+++ trunk/fat32_test.X/build/default/production/main.p1.d
@@ -0,0 +1,12 @@
+ build/default/production/main.d  \
+ build/default/production/main.p1:  \
+ main.c  \
+../my_lib.X/rosso_delays.h  \
+../my_lib.X/rosso.h  \
+../my_lib.X/rosso_hwspi2.h  \
+../my_lib.X/rosso_sdcard.h  \
+../my_lib.X/delays.h  \
+../my_lib.X/pic18f46k22.h  \
+../my_lib.X/rosso_hwserial.h  \
+../my_lib.X/rosso_f32.h  \
+../my_lib.X/TypeDefs.h 
