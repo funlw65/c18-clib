f03fc4a9a49e41bd08b8c79ca2f48f3afb89800f
Vasi <funlw65@gmail.com>
2015-03-28 06:56:23 +0200

    corrected the template file

--- my_lib.X/main_sample.c
+++ my_lib.X/main_sample.c
@@ -16,41 +16,14 @@
 
 // declaring the headers for the interrupt functions
 void
-#ifdef __XC8
 interrupt
-#endif
 high_isr(void);
 
 void
-#ifdef __XC8
 interrupt low_priority
-#endif
 low_isr(void);
 
-#ifdef __18CXX
-#ifndef NOBOOT
-#pragma romdata bootloader = 0x06
-const rom char bootloader[APP_START - 0x06];
-extern void _startup(void);
-#pragma code AppVector = APP_START
-void AppVector(void) {
-    _asm GOTO _startup _endasm
-}
-#pragma code AppHighIntVector = APP_HINT
-void AppHighIntVector(void) {
-    _asm
-        GOTO high_isr // branch to the high_isr()
-    _endasm
-}
-#pragma code AppLowIntVector = APP_LINT
-void low_vector(void) {
-    _asm
-        GOTO low_isr // branch to the low_isr()
-    _endasm
-}
-#endif
-#pragma code  // Let this code pragma
-#endif // to be close to main function with nothing between!!!
+
 void main() {
     AllDigital(); // all pins digital
 #ifdef ONBOARD
@@ -65,24 +38,15 @@ void main() {
     }
 }
 
-#ifdef __18CXX
-#pragma interrupt high_isr
-#endif
 void
-#ifdef __XC8
 interrupt
-#endif
 high_isr(void) {
     //
 }
 
-#ifdef __18CXX
-#pragma interruptlow low_isr
-#endif
+
 void
-#ifdef __XC8
 interrupt low_priority
-#endif
 low_isr(void) {
     //
 }
