291fcd5e0e5600da50cbdbb38c616aad0a6be3d9
Vasi <funlw65@gmail.com>
2016-05-01 14:53:32 +0300

    Cosmetic changes for the main template.

--- my_lib.X/main_sample.c
+++ my_lib.X/main_sample.c
@@ -6,47 +6,36 @@
  */
 
 #include <TypeDefs.h>
-//#define NOBOOT 1 // uncomment if you don't use a bootloader
-// disable following line if your hardware differs
-#define ONBOARD 1 // enable the default onboard definitions
-#include <rosso.h> // processor type, speed, configuration bits, hardware, app_offset.
-// add here other definitions and include header files if needed
+/* #define NOBOOT 1 */ /*uncomment if you don't use a bootloader*/
+/* disable following line if your hardware differs */
+#define ONBOARD 1 /* enable the default onboard definitions*/
+#include <rosso.h> /* processor type, speed, configuration bits, hardware, app_offset.*/
+/* add here other definitions and include header files if needed*/
 
-// add here your global variables, definitions and other fuctions you may need
+/* add here your global variables, definitions and other functions you may need*/
 
-// declaring the headers for the interrupt functions
-void
-interrupt
-high_isr(void);
-
-void
-interrupt low_priority
-low_isr(void);
-
+/* declaring the headers for the interrupt functions */
+void interrupt high_isr(void);
+void interrupt low_priority low_isr(void);
 
 void main() {
-    AllDigital(); // all pins digital
+    AllDigital(); /* all pins digital*/
 #ifdef ONBOARD
-    OnBoardLED_dir = 0; // set pin as output
-    OnBoardButton_dir = 1; // set pin as input
-    OnBoardLED = 0; // set the LED off
+    OnBoardLED_dir = 0; /* set pin as output */
+    OnBoardButton_dir = 1; /* set pin as input */
+    OnBoardLED = 0; /* set the LED off */
 #endif
-    //sei(); // enable general interrupts if needed
-    // Add other initializations you may have...
+    /*sei();*/ /* enable general interrupts if needed*/
+    /* Add other initializations you may have...*/
     while (1) {
-        // Add your repeating code...
+        /* Add your repeating code...*/
     }
 }
 
-void
-interrupt
-high_isr(void) {
-    //
+void interrupt high_isr(void) {
+    /* add code if needed */
 }
 
-
-void
-interrupt low_priority
-low_isr(void) {
-    //
+void interrupt low_priority low_isr(void) {
+    /* add code if needed */
 }
